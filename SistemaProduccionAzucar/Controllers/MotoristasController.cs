39641436481248d4b7492ba6f2305484dedda89f
CJRamirez4 <93959172+CJRamirez4@users.noreply.github.com>
2021-11-28 01:45:35 -0600

    Pantalla de Motoristas
    Agregue pantalla

--- SistemaProduccionAzucar/Controllers/MotoristasController.cs
+++ SistemaProduccionAzucar/Controllers/MotoristasController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SistemaProduccionAzucar.Models.Motoristas;
+using SistemaProduccionAzucar.Models.TableViewModels;
 
 namespace SistemaProduccionAzucar.Controllers
 {
@@ -12,7 +14,84 @@ namespace SistemaProduccionAzucar.Controllers
         // GET: /Motoristas/
         public ActionResult Index()
         {
-            return View();
+            List<Motoristas> list = null;
+
+            using (MotoristasEntities db = new MotoristasEntities())
+            {
+                list = (from m in db.motoristas
+                        orderby m.idDUI ascending
+                        select new Motoristas
+                        {
+                            idDUI = m.idDUI,
+                            motorista = m.motorista,
+                            edadMot = m.edadMot,
+                            genMot = m.genMot,
+                            tipoLicMot = m.tipoLicMot,
+                            placa = m.placa,
+                            Estado = m.Estado,
+                        }).ToList();
+            }
+
+            return View(list);
         }
-	}
+
+        public ActionResult AgregarMotoristas(int idDUI, string motorista, int edad, string genero, string tipoLic, string placa, int Estado)
+        {
+            using (MotoristasEntities db = new MotoristasEntities())
+            {
+                var list = from m in db.motoristas
+                           where m.idDUI == idDUI
+                           select m;
+
+                if (list.Count() > 0)
+                {
+                    return Json(new
+                    {
+                        response = 0,
+                        message = "Motorista ya existe."
+                    });
+                }
+                else
+                {
+                    motoristas motor = new motoristas();
+                    motor.idDUI = idDUI;
+                    motor.motorista = motorista;
+                    motor.edadMot = edad;
+                    motor.genMot = genero;
+                    motor.tipoLicMot = tipoLic;
+                    motor.placa = "";
+                    motor.Estado = 1;
+
+                    db.motoristas.Add(motor);
+                    db.SaveChanges();
+
+                    return Json(new
+                    {
+                        response = 1,
+                        message = "Éxito"
+                    });
+
+                }
+            }
+        }
+
+        public ActionResult CambEstado(string id_motorista)
+        {
+            using (MotoristasEntities db = new MotoristasEntities())
+            {
+                motoristas mot = db.motoristas.Find(id_motorista);
+                mot.Estado = (mot.Estado == 1 ? 0 : 1);
+
+                db.Entry(mot).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+
+                return Json(new
+                {
+                    response = 1,
+                    message = "Éxito"
+                });
+
+            }
+        }
+    }
 }
---
+++ SistemaProduccionAzucar/Models/TableViewModels/Motoristas.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SistemaProduccionAzucar.Models.TableViewModels
+{
+    public class Motoristas
+    {
+        public int idDUI { get; set; }
+        public string motorista { get; set; }
+        public int edadMot { get; set; }
+        public string genMot { get; set; }
+        public string tipoLicMot { get; set; }
+        public string placa { get; set; }
+        public int Estado { get; set; }
+    }
+}
